29fd4c91bbbcba8159db96c2401ce888a3a71c46
Nihalrohira <143147647+Nihalrohira@users.noreply.github.com>
2026-04-23 15:08:30 +0530

    fix: static export for cloudflare

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,12 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",   // 👈 ADD THIS LINE
+
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
